7f61c9a2f600fd3544d4cc4a9a868eb53c0c8228
Adeja <alendor6@gmail.com>
2020-05-12 10:56:16 -0400

    add state.user.username to display current user

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
+
 const Home = () => {
-  const username = useSelector(state => state.username);
+  const username = useSelector(state => state.user.username);
   const text = username ? (
     <h1>{username} is currently logged in</h1>
   ) : (
@@ -10,5 +11,7 @@ const Home = () => {
   );
   return <div>{text}</div>;
 };
+
+/* const mapStateToProps = (state) => ({username: state.username}) */
 
 export default Home;
