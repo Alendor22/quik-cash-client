a840d172e20d570f43d0ebb598e3c15884fb2b8e
Adeja <alendor6@gmail.com>
2020-05-13 18:39:04 -0400

    add route to listings/new

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Pages from './Pages'
+import CreateListing from './Components/CreateListing'
 
 const Routes = () => {
   return (
@@ -8,6 +9,7 @@ const Routes = () => {
         <Route exact path="/" component={Pages.Home} />
         <Route exact path="/signup" component={Pages.Signup} />
         <Route exact path="/login" component={Pages.Login} />
+        <Route exact path="/listings/new" component={CreateListing} />
       </Switch>
   )
 }
