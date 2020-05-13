00d63ece2302118cd79ea4e9d7190ffcb3bcd2a8
Adeja <alendor6@gmail.com>
2020-05-13 18:38:40 -0400

    add h1 and fragments

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom'
 import Routes from './Routes'
 import Nav from './Components/Nav'
+import CreateListing from './Components/CreateListing'
 
 function App() {
   return (
-      
+    <>
+    <h1>QuikCash</h1>
       <Router >
         <Nav />
+        
         <Routes />
       </ Router >
+    </>
   );
 }
 
