926c2cb75f882ddaed404ba087450fef65d53570
Adeja <alendor6@gmail.com>
2020-05-16 14:16:14 -0400

    add componentDidMount to file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React from 'react';
-import {BrowserRouter as Router} from 'react-router-dom'
-import Routes from './Routes'
-import Nav from './Components/Nav'
-import CreateListing from './Components/CreateListing'
+import React, { Component } from 'react';
+import Routes from './Routes';
+import Nav from './Components/Nav';
+import { loadListingsIndex } from './redux/actions';
+import { connect } from 'react-redux';
+import Pages from './Pages';
+import { withRouter } from 'react-router-dom';
+import { ListingShowContainer } from './Containers/ListingShowContainer';
 
-function App() {
+class App extends Component {
+
+  componentDidMount () {
+    this.props.loadListingsIndex();
+    this.props.history.push("/listings");
+  }
+  
+  render() {
   return (
     <>
     <h1>QuikCash</h1>
-      <Router >
         <Nav />
-        
+        <Pages.Home />
+        <ListingShowContainer />
         <Routes />
-      </ Router >
     </>
-  );
+    );
+  }
 }
 
-export default App;
+export default withRouter(connect(null, { loadListingsIndex })(App));
