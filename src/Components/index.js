02d1bcac8151a230ad8dbae91a2f296f3fe5294e
Adeja <alendor6@gmail.com>
2020-05-16 14:19:44 -0400

    create index file for components

---
+++ src/Components/index.js
@@ -0,0 +1,13 @@
+import BuyListing from './BuyListing';
+import CreateListing from './CreateListing';
+import FavoriteListing from  './FavoriteListing';
+import ListingsLinks from './ListingsLinks';
+import ListingLink from './ListingsLinks';
+
+export default {
+  BuyListing,
+  CreateListing,
+  FavoriteListing,
+  ListingsLinks,
+  ListingLink
+};
