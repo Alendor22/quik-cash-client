f48f2b68b0f909bcb4f216cfcc594906f4051deb
Adeja <alendor6@gmail.com>
2020-05-16 14:18:10 -0400

    add <Route> to /listings, listings/listings.id

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import Pages from './Pages'
-import CreateListing from './Components/CreateListing'
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Pages from './Pages';
+import Components from './Components/';
+
 
 const Routes = () => {
   return (
       <Switch>
-        <Route exact path="/" component={Pages.Home} />
         <Route exact path="/signup" component={Pages.Signup} />
         <Route exact path="/login" component={Pages.Login} />
-        <Route exact path="/listings/new" component={CreateListing} />
+        <Route exact path="/listings/new" component={Components.CreateListing} />
+        <Route exact path="/listings" component={Components.ListingsContainer} />
+        <Route exact path="/listings/listings.id" component={Components.ListingShowContainer} />
       </Switch>
   )
 }
