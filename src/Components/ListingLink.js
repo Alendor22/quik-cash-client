b0e6c5d16e8328dd84a2c203ec231ac6fbaf3045
Adeja <alendor6@gmail.com>
2020-05-19 21:52:08 -0400

    add logic to render a listingLink

--- src/Components/ListingLink.js
+++ src/Components/ListingLink.js
@@ -1,21 +1,30 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import BuyListing from './BuyListing'
 import { Link } from 'react-router-dom';
 
 export const ListingLink = (props) => {
-    const renderListingLink = () => {
-        return <div key={listing.id}>
-                  <Link key={listing.id} to={`/listings/${listing.id}`}>
-                    {listing.item_name}
-                    {listing.description}
-                    {listing.price}
-                    </Link>
+  const { key } = props;
+  const listings = useSelector(state => state.listings);
+  const listing = listings.find(listing => listing.id === parseInt(props.location.pathname.split('/listings/')[1]));
+      const renderListingLink = () => {
+      
+        return <div>
+                  <span key={key} to={`/listings/${key}`}>
+                    <p>Item Name - {listing.item_name}</p>
+                    <p>Description - {listing.description}</p>
+                    <p>Price - ${listing.price}</p>
+                    <p>Seller - <Link to={`/users/${listing.seller_id}/listings`}>{listing.seller.username}</Link></p>
+                    <p>Buyer - <Link to={`/users/${listing.buyer_id}/listings`}>{listing.sold === false ? <>This listing is available for purchase!</> : listing.buyer.username}</Link></p>
+                  </span>
+                  <>{listing.sold === false ? <BuyListing /> : <>This listing in no longer available!</>}</>
                </div>
     }
     
 
     return (
       <ul>
-        {renderListingLink()}
+         {listing ? renderListingLink() : <p>loading...</p>}
       </ul>
     )
 
