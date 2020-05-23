08225f6cd5d8fc8b500d2637b3224d9249d96105
Adeja <alendor6@gmail.com>
2020-05-23 00:23:24 -0400

    add NavLink for /listings

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,22 +1,29 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import userActions from '../redux/actions';
+import { NavLink } from 'react-router-dom';
 
-const Nav = () => {
-  const dispatch = useDispatch();
-  const handleLogout = () => {
-    dispatch(userActions.logoutUser());
-  };
+  
+  const Nav = () => {
+    //const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
+    const dispatch = useDispatch();
+    const handleLogout = () => {
+      dispatch(userActions.logoutUser());
+    };
+
+
+  // const isLoggedIn = localStorage.token !== "" && localStorage.token !== undefined && localStorage.token !== "undefined" && localStorage.token !== "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.JEdfpl510uWu_3NaqmgTGJdn1j8uZS7V2Omws-GdxRk";
+
   return (
     <nav style={{ display: 'flex', justifyContent: 'space-evenly' }}>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/signup">Signup</NavLink>
-      <NavLink to="/login">Login</NavLink>
-      <NavLink to="/listings/new">Add Listing</NavLink>
-      <NavLink to="/" onClick={handleLogout}>Logout</NavLink>
+        <NavLink to="/listings">Home</NavLink>
+        <NavLink to="/signup">Signup</NavLink>
+        <NavLink to="/login">Login</NavLink>
+        <NavLink to="/listings/new">Post Listing</NavLink>
+        <NavLink to="/" onClick={handleLogout}>Logout</NavLink>
     </nav>
   );
-};
+}
+
 
 export default Nav;
