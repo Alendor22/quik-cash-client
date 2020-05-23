65e924741eeedcd28b71f1b2d044c7054b754522
Adeja <alendor6@gmail.com>
2020-05-23 00:22:31 -0400

    add logic to control when the button appears

--- src/Components/ListingLink.js
+++ src/Components/ListingLink.js
@@ -3,24 +3,29 @@ import { useSelector } from 'react-redux';
 import BuyListing from './BuyListing'
 import { Link } from 'react-router-dom';
 
+
 export const ListingLink = (props) => {
   const { key } = props;
   const listings = useSelector(state => state.listings);
+  const token = localStorage.getItem('token');
+  const isLoggedIn = !!token;
   const listing = listings.find(listing => listing.id === parseInt(props.location.pathname.split('/listings/')[1]));
+  const buyerOfListing = listing && listing.buyer && listing.buyer.username;
+  const listingAvail = listing && !listing.sold;
+
       const renderListingLink = () => {
       
-        return <div>
+        return <>
                   <span key={key} to={`/listings/${key}`}>
                     <p>Item Name - {listing.item_name}</p>
                     <p>Description - {listing.description}</p>
                     <p>Price - ${listing.price}</p>
                     <p>Seller - <Link to={`/users/${listing.seller_id}/listings`}>{listing.seller.username}</Link></p>
-                    <p>Buyer - <Link to={`/users/${listing.buyer_id}/listings`}>{listing.sold === false ? <>This listing is available for purchase!</> : listing.buyer.username}</Link></p>
+                    <p>Buyer - <Link to={`/users/${listing.buyer_id}/listings`}>{listingAvail && isLoggedIn ? <BuyListing listing = {listing}/> : buyerOfListing}</Link></p>
                   </span>
-                  <>{listing.sold === false ? <BuyListing /> : <>This listing in no longer available!</>}</>
-               </div>
+               </>
     }
-    
+
 
     return (
       <ul>
