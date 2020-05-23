cad705f08d9ca1a10e9931c80849b846852a0e82
Adeja <alendor6@gmail.com>
2020-05-23 00:21:55 -0400

    remove FavoriteListing export

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,6 +1,6 @@
 import BuyListing from './BuyListing';
 import CreateListing from './CreateListing';
-import FavoriteListing from  './FavoriteListing';
+//import FavoriteListing from  './FavoriteListing';
 import ListingsIndex from './ListingsIndex';
 import ListingLink from './ListingLink';
 import UsersListingsIndex from './UsersListingsIndex';
@@ -8,7 +8,6 @@ import UsersListingsIndex from './UsersListingsIndex';
 export default {
   BuyListing,
   CreateListing,
-  FavoriteListing,
   ListingsIndex,
   ListingLink,
   UsersListingsIndex
