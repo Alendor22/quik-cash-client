d1ec81c50d3edb567eb9bd49c1abf4c8b01cb8ca
Adeja <alendor6@gmail.com>
2020-05-25 19:48:21 -0400

    add link to /listings/listing.seller_id

--- src/Components/UsersListingsIndex.js
+++ src/Components/UsersListingsIndex.js
@@ -2,20 +2,21 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link, withRouter } from 'react-router-dom';
 
-
 export const UsersListingsIndex = (routerProps) => {
-      const usersListings = useSelector(state => state.listings.filter(listings => listings.seller_id === parseInt(routerProps.match.params.id)));
+      const userListings = useSelector(state => state.listings.filter(listings => listings.seller_id === parseInt(routerProps.match.params.id)));
       const renderUsersListingsIndex = () => {
-        return usersListings.map(listing => {
+        return userListings.map(listing => {
           return  <div key={listing.id}>
-                      <Link key={listing.id} to={`users/${listing.seller_id}/listings`}>{listing.item_name}</Link>
+                      <Link key={listing.id} to={`listings/${listing.seller_id}`}>Seller: {listing.seller.username} - Listing: {listing.item_name}</Link>
+                      
                   </div>
+                    
         })
     }
     
     return (
       <ul>
-         {renderUsersListingsIndex()}
+          {renderUsersListingsIndex()} 
       </ul>
     )
 
