b81e788e09349502160c22d247ff2f2ba20822d8
Adeja <alendor6@gmail.com>
2020-05-25 19:41:26 -0400

    add UsersListings

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,12 +1,15 @@
 import BuyListing from './BuyListing';
+import UsersListings from './UsersListings';
 import CreateListing from './CreateListing';
 //import FavoriteListing from  './FavoriteListing';
 import ListingsIndex from './ListingsIndex';
 import ListingLink from './ListingLink';
 import UsersListingsIndex from './UsersListingsIndex';
+import UsersListingsButton from './UsersListingsButton'
 
 export default {
   BuyListing,
+  UsersListings,
   CreateListing,
   ListingsIndex,
   ListingLink,
