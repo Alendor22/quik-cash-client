db90a0d9b78eb7b921f242f3bfd22358486326ab
Adeja <alendor6@gmail.com>
2020-05-25 19:43:51 -0400

    add ternary to check if a user is logged in

--- src/Components/ListingsIndex.js
+++ src/Components/ListingsIndex.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+//import UsersListings from './UsersListings';
+import UsersListings  from './UsersListings';
 
 export const ListingsIndex = () => {
   const listings = useSelector(state => state.listings);
+  const token = localStorage.getItem('token');
+  const isLoggedIn = !!token;
     const renderListingsIndex = () => {
   
      return listings.map(listing => {
-        return <div key={listing.id}>
+        return  <div key={listing.id}>
                   <Link key={listing.id} to={`/listings/${listing.id}`}>{listing.item_name}</Link>
-               </div>
+                </div>
+               
       })
     }
 
     return (
+      <div>
       <ul>
-        {renderListingsIndex()}
+        <h3>Quik-Listings</h3>
+          {renderListingsIndex()}
       </ul>
+        <h3>My-Quik-Listings</h3>
+        {isLoggedIn ? <UsersListings /> : "Please login to see your listings!" }
+      </div>
     )
 
 }
