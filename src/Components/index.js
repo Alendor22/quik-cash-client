cd14013732e837755ee36e88559d03e2bbeb1921
Adeja <alendor6@gmail.com>
2020-06-03 00:32:51 -0400

    add imports for components

--- src/Components/index.js
+++ src/Components/index.js
@@ -4,14 +4,18 @@ import CreateListing from './CreateListing';
 import ListingsIndex from './ListingsIndex';
 import ListingLink from './ListingLink';
 import UsersListingsIndex from './UsersListingsIndex';
-import UsersBoughtListings from './UsersBoughtListings'
+import UsersBoughtListings from './UsersBoughtListings';
+import AvailableListings from './AvailableListings';
+import UsersListingsLink from './UsersListingsLink';
 
 export default {
+  AvailableListings,
   BuyListing,
   UsersListings,
   CreateListing,
   ListingsIndex,
   ListingLink,
   UsersBoughtListings,
-  UsersListingsIndex
+  UsersListingsIndex,
+  UsersListingsLink
 };
