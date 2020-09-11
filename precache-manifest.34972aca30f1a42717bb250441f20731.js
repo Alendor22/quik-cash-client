de82dfea65da5908d31735702524c5d4ac901beb
Alendor22 <alendor6@gmail.com>
2020-09-11 09:54:03 -0400

    Updates

---
+++ precache-manifest.34972aca30f1a42717bb250441f20731.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "255475d936c39763b8f2a954e41bf89f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1649e2dcffb87686034d",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6aeda656a681d4021ff7",
+    "url": "/static/css/main.c2d745fc.chunk.css"
+  },
+  {
+    "revision": "1649e2dcffb87686034d",
+    "url": "/static/js/2.2fc66160.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2fc66160.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aeda656a681d4021ff7",
+    "url": "/static/js/main.712d7c48.chunk.js"
+  },
+  {
+    "revision": "b73b5d000afcc75a861d",
+    "url": "/static/js/runtime-main.38facfbb.js"
+  }
+]);
