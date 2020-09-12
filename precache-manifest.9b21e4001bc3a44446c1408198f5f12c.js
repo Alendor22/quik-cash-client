182c1a962e9ae7c230722eec1c72d5af832f2890
Alendor22 <alendor6@gmail.com>
2020-09-12 13:14:34 -0400

    github deploy code

---
+++ precache-manifest.9b21e4001bc3a44446c1408198f5f12c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f3ab34bf6ffe1bf9f1f252425c1497",
+    "url": "/quik-cash-client/index.html"
+  },
+  {
+    "revision": "1649e2dcffb87686034d",
+    "url": "/quik-cash-client/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d297495d0dd1d15cebf6",
+    "url": "/quik-cash-client/static/css/main.c2d745fc.chunk.css"
+  },
+  {
+    "revision": "1649e2dcffb87686034d",
+    "url": "/quik-cash-client/static/js/2.2fc66160.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/quik-cash-client/static/js/2.2fc66160.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d297495d0dd1d15cebf6",
+    "url": "/quik-cash-client/static/js/main.5026a92a.chunk.js"
+  },
+  {
+    "revision": "db5f35f6665719b83bd9",
+    "url": "/quik-cash-client/static/js/runtime-main.e2acd6ad.js"
+  }
+]);
