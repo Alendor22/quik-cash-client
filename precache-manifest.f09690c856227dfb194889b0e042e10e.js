2215910050852b1aa891a2341fe332df5c5f1d4c
Alendor22 <alendor6@gmail.com>
2020-09-13 10:35:47 -0400

    3rd attempt to clean git history

---
+++ precache-manifest.f09690c856227dfb194889b0e042e10e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e43cffc42eba470180629c77671081",
+    "url": "/quik-cash-client/index.html"
+  },
+  {
+    "revision": "1649e2dcffb87686034d",
+    "url": "/quik-cash-client/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "388fd1139736b852914c",
+    "url": "/quik-cash-client/static/css/main.c2d745fc.chunk.css"
+  },
+  {
+    "revision": "1649e2dcffb87686034d",
+    "url": "/quik-cash-client/static/js/2.2fc66160.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/quik-cash-client/static/js/2.2fc66160.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "388fd1139736b852914c",
+    "url": "/quik-cash-client/static/js/main.7ce80d38.chunk.js"
+  },
+  {
+    "revision": "db5f35f6665719b83bd9",
+    "url": "/quik-cash-client/static/js/runtime-main.e2acd6ad.js"
+  }
+]);
